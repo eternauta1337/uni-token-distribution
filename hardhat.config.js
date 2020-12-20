975fd6812e2c76d15ae0081469b0e9265a01c207
Alejandro <palebluedot@gmail.com>
2020-12-20 09:07:28 -0300

    Script can check all claims

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,7 +19,15 @@ task('unicheck', 'Checks if an account/s can claim UNI rewards')
     console.log(gray(`  > Tokens to claim: ${hexToNumber(data.tokenTotal)}`));
     console.log(gray(`  > Accounts that can claim: ${Object.keys(data.claims).length}`));
 
-    const accounts = taskArguments.accounts.split(',');
+    let accounts = taskArguments.accounts;
+    if (accounts === ',') {
+      accounts = Object.keys(data.claims).join(',');
+    }
+
+    accounts = accounts.split(',');
+    console.log(gray(`  > Checking ${accounts.length} accounts...`));
+
+    let toBeClaimed = hasBeenClaimed = canClaim = haveClaimed = 0;
     for (let i = 0; i < accounts.length; i++) {
       const account = ethers.utils.getAddress(accounts[i]);
       console.log(cyan(`Checking account ${account}`));
@@ -35,7 +43,7 @@ task('unicheck', 'Checks if an account/s can claim UNI rewards')
           new ethers.providers.JsonRpcProvider(process.env.PROVIDER) :
           ethers.getDefaultProvider();
 
-        console.log(gray(`  > Querying TokenDistributor.isClaimed(${account})...`));
+        console.log(gray(`  > Querying TokenDistributor.isClaimed(${entry.index})...`));
         const TokenDistributor = new ethers.Contract(
           '0x090d4613473dee047c3f2706764f49e0821d256e',
           require('./abis/TokenDistributor.json'),
@@ -45,13 +53,23 @@ task('unicheck', 'Checks if an account/s can claim UNI rewards')
         const isClaimed = await TokenDistributor.isClaimed(entry.index);
         if (isClaimed) {
           console.log(gray(`  > Account has already claimed ${amount} UNI`));
+
+          haveClaimed++;
+          hasBeenClaimed += amount;
         } else {
           console.log(green(`  > Account has ${amount} UNI to claim!`));
+
+          canClaim++;
+          toBeClaimed += amount;
         }
       } else {
         console.log(gray('  > Account has no UNI to claim :('));
       }
     }
+
+    console.log(cyan(`Summary:`));
+    console.log(gray(`  > ${canClaim} accounts can claim ${toBeClaimed} UNI`));
+    console.log(gray(`  > ${haveClaimed} accounts have claimed ${hasBeenClaimed} UNI`));
   });
 
 module.exports = {
